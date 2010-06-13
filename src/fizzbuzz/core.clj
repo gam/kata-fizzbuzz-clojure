af2ce657b30d02296ebbdd7aa140b042517b1033
Geir Amdal <gam@grok.no>
2010-06-13 22:51:20 +0200

    first attempt, first part

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -1,2 +1,11 @@
 (ns fizzbuzz.core)
 
+(defn- divisible-by? [number divisor]
+  (zero? (mod number divisor)))
+
+(defn fizzbuzz [number]
+  (cond
+   (divisible-by? number 15) "fizzbuzz"
+   (divisible-by? number 3) "fizz"
+   (divisible-by? number 5) "buzz"
+   :else number))
--- test/fizzbuzz/core_test.clj
+++ test/fizzbuzz/core_test.clj
@@ -2,3 +2,11 @@
   (:use [fizzbuzz.core] :reload-all)
   (:use [clojure.test]))
 
+(deftest fizzbuzz-test
+  (is (= (fizzbuzz 1) 1))
+  (is (= (fizzbuzz 2) 2))
+  (is (= (fizzbuzz 3) "fizz"))  
+  (is (= (fizzbuzz 5) "buzz"))
+  (is (= (fizzbuzz 6) "fizz"))
+  (is (= (fizzbuzz 10) "buzz"))
+  (is (= (fizzbuzz 15) "fizzbuzz")))
