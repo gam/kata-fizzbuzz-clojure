d1147f9c857b4ecab8f7af2027c28d73dda7275a
Geir Amdal <gam@grok.no>
2010-06-13 21:35:05 +0200

    added dependency to clojure-refactoring

--- project.clj
+++ project.clj
@@ -5,13 +5,14 @@
     and for the multiples of five print "Buzz". 
     For numbers which are multiples of both three and five print 'FizzBuzz'.
 
-	A further requirement for the kata: 
-	The FizzBuzz generator should be programmable; introducing new 
-	substitutions should be as simple as introducing a new rule. 
-	
-	As an example, expand the FizzBuzz generator with the following behaviour:
-	Numbers divisible by two are replaced by 'coconut' and numbers divisible 
-	by seven are replaced by 'banana'."
+    A further requirement for the kata: 
+    The FizzBuzz generator should be programmable; introducing new 
+    substitutions should be as simple as introducing a new rule.
+
+    As an example, expand the FizzBuzz generator with the following behaviour:
+    Numbers divisible by two are replaced by 'coconut' and numbers divisible 
+    by seven are replaced by 'banana'."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]
+		     [clojure-refactoring "0.1.5"]])
