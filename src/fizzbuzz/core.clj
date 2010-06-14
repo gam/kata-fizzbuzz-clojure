77986addbaa29c53e57aba0356b210978f398f66
Geir Amdal <gam@grok.no>
2010-06-14 22:04:10 +0200

    extract function to clean up condp form

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -1,8 +1,10 @@
 (ns fizzbuzz.core)
 
+(defn- is-divisible-by? [divisor number]
+  (zero? (mod number divisor)))
+
 (defn fizzbuzz [number]
-  (condp
-      #(zero? (mod %2 %1)) number
+  (condp is-divisible-by? number
     3 "fizz"
     5 "buzz"
     number))
