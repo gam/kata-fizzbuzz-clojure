522aad96df273bc69177669e22f39399b87d5089
Geir Amdal <gam@grok.no>
2010-06-14 20:45:46 +0200

    added rule based processing

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -3,9 +3,22 @@
 (defn- divisible-by? [number divisor]
   (zero? (mod number divisor)))
 
+(defn- create-rule [divisor replacement]
+  (fn [number]
+    (if (divisible-by? number divisor) replacement)))
+
+(defn- apply-rules [rules number]
+  (apply str ((apply juxt rules) number)))
+
+(defn- perform-substitution [rules number]
+  (let [substitute (apply-rules rules number)]
+    (if (empty? substitute) number substitute)))
+
+(defn- fizzbuzz-rules []
+  [(create-rule 3 "fizz")
+   (create-rule 5 "buzz")]
+
 (defn fizzbuzz [number]
-  (cond
-   (divisible-by? number 15) "fizzbuzz"
-   (divisible-by? number 3) "fizz"
-   (divisible-by? number 5) "buzz"
-   :else number))
+    (perform-substitution (fizzbuzz-rules) number)))
+	
+
--- test/fizzbuzz/core_test.clj
+++ test/fizzbuzz/core_test.clj
@@ -3,6 +3,10 @@
   (:use [clojure.test]))
 
 (deftest fizzbuzz-test
+  "Tests for the fizzbuzz resolver:
+  For multiples of 3, return 'fizz'.
+  For multiples of 5, return 'buzz'.
+  For multiples of 3 AND 5, return 'fizzbuzz'."
   (is (= (fizzbuzz 1) 1))
   (is (= (fizzbuzz 2) 2))
   (is (= (fizzbuzz 3) "fizz"))  
@@ -10,3 +14,4 @@
   (is (= (fizzbuzz 6) "fizz"))
   (is (= (fizzbuzz 10) "buzz"))
   (is (= (fizzbuzz 15) "fizzbuzz")))
+
