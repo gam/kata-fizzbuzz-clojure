022003b9b32e8eb2e345d8afea741cecb716ceaa
Geir Amdal <gam@grok.no>
2010-06-14 21:24:56 +0200

    second attempt, first step

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -1,2 +1,4 @@
 (ns fizzbuzz.core)
 
+(defn fizzbuzz [number]
+  (if (= number 3) "fizz" number))
--- test/fizzbuzz/core_test.clj
+++ test/fizzbuzz/core_test.clj
@@ -2,3 +2,8 @@
   (:use [fizzbuzz.core] :reload-all)
   (:use [clojure.test]))
 
+(deftest fizzbuzz-test
+  (is (= (fizzbuzz 1) 1))
+  (is (= (fizzbuzz 2) 2))
+  (is (= (fizzbuzz 3) "fizz"))) 
+    
