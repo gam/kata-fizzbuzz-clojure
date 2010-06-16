e8a1c53230bd01bf1f8ca0c17432d8c3f611291e
Geir Amdal <gam@grok.no>
2010-06-17 00:57:21 +0200

    1 thru 15 complete

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -1,2 +1,8 @@
 (ns fizzbuzz.core)
 
+(defn fizzbuzz [number]
+  (condp #(zero? (mod %2 %1)) number
+    15 "fizzbuzz"
+    3 "fizz"
+    5 "buzz"
+    number))
--- test/fizzbuzz/core_test.clj
+++ test/fizzbuzz/core_test.clj
@@ -2,3 +2,21 @@
   (:use [fizzbuzz.core] :reload-all)
   (:use [clojure.test]))
 
+(deftest fizzbuzz-test
+  (are [number expected]
+       (= (fizzbuzz number) expected)
+       1 1
+       2 2
+       3 "fizz"
+       4 4
+       5 "buzz"
+       6 "fizz"
+       7 7
+       8 8
+       9 "fizz"
+       10 "buzz"
+       11 11
+       12 "fizz"
+       13 13
+       14 14
+       15 "fizzbuzz"))
