5fed6071581a1a2a32643b24c68831ae6536ca70
Geir Amdal <gam@grok.no>
2010-07-23 01:15:39 +0200

    Final state, 5th attempt

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -1,2 +1,24 @@
 (ns fizzbuzz.core)
 
+(def *fizzbuzz-rules*
+  [(create-rule 3 "fizz")
+   (create-rule 5 "buzz")])
+
+(defn- is-divisible-by [divisor number]
+  (zero? (mod number divisor)))
+
+(defn- apply-rules [rules number]
+  (apply str ((apply juxt rules) number)))
+
+(defn create-rule [divisor replacement]
+  (fn [number]
+    (when (is-divisible-by divisor number) replacement)))
+
+(defn fizzbuzz
+  ([number]
+     (fizzbuzz *fizzbuzz-rules* number))
+  ([rules number]
+     (let [replacement (apply-rules rules number)]
+       (if (empty? replacement) number replacement))))
+
+
--- test/fizzbuzz/core_test.clj
+++ test/fizzbuzz/core_test.clj
@@ -2,3 +2,39 @@
   (:use [fizzbuzz.core] :reload-all)
   (:use [clojure.test]))
 
+(deftest fizzbuzz-test
+  (are [number expected]
+       (= (fizzbuzz number) expected)
+       1 1
+       2 2
+       3 "fizz"
+       4 4
+       5 "buzz"
+       6 "fizz"
+       7 7
+       8 8
+       9 "fizz"
+       10 "buzz"
+       11 11
+       12 "fizz"
+       13 13
+       14 14
+       15 "fizzbuzz"))
+
+(deftest rule-test
+  (let [fizz-rule (create-rule 3 "fizz")
+	buzz-rule (create-rule 5 "buzz")]
+    (testing "fizz-rule"
+      (is (= (fizz-rule 3) "fizz"))
+      (is (= (fizz-rule 2) nil)))
+    (testing "buzz-rule"
+      (is (= (buzz-rule 5) "buzz"))
+      (is (= (buzz-rule 3) nil)))))
+
+(deftest fizzbuzz-rules-test
+  (let [rules [(create-rule 2 "pineapple")
+	       (create-rule 7 "grapefruit")]]
+    (is (= (fizzbuzz rules 2) "pineapple"))
+    (is (= (fizzbuzz rules 7) "grapefruit"))
+    (is (= (fizzbuzz rules 14) "pineapplegrapefruit"))
+    (is (= (fizzbuzz rules 3) 3))))
