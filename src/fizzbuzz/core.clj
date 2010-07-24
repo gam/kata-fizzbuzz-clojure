619b397b9cc23fdd470126cd2d13edd7316cbaf6
Geir Amdal <gam@grok.no>
2010-07-24 17:23:28 +0200

    rule-test added

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -2,10 +2,15 @@
 
 (defn- divisible-by [divisor number]
   (zero? (mod number divisor)))
-   
+
+(defn create-rule [divisor replacement]
+  (fn [number]
+    (when (divisible-by divisor number) replacement)))
+  
 (defn fizzbuzz [number]
   (condp divisible-by number
     15 "fizzbuzz"
     3 "fizz"
     5 "buzz"
     number))
+
