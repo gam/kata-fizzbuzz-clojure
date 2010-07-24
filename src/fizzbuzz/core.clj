3d2cec24b2c641031146569624a98cd9fa6e322e
Geir Amdal <gam@grok.no>
2010-07-24 15:04:05 +0200

    modulus operator introduced for fizzbuzz of 6 - before refactoring

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -1,7 +1,7 @@
 (ns fizzbuzz.core)
 
 (defn fizzbuzz [number]
-  (cond
-   (= number 3) "fizz"
-   (= number 5) "buzz"
-   :true number))
+  (condp #(zero? (mod %2 %1)) number
+      3 "fizz"
+      5 "buzz"
+      number))
