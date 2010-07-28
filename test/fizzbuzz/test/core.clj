ea2a745b5b0ec8c07f8d2de59e9a475849f5ea7d
Geir Amdal <gam@grok.no>
2010-07-28 23:48:14 +0200

    tests refactored: using (are... instead of multiple (is...

--- test/fizzbuzz/test/core.clj
+++ test/fizzbuzz/test/core.clj
@@ -3,5 +3,6 @@
   (:use [clojure.test]))
 
 (deftest fizzbuzz-test
-  (is (= (fizzbuzz 1) 1) "one is one")
-  (is (= (fizzbuzz 2) 2) "two is two"))
+  (are [number expected] (= (fizzbuzz number) expected)
+       1 1
+       2 2))
