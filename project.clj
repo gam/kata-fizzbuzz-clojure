9047fe93b6d5c07dacb2bb2945c88ad27bc88eff
Geir Amdal <gam@grok.no>
2010-08-01 20:40:49 +0200

    Updated clojure dependencies to 1.2.0-RC1

--- project.clj
+++ project.clj
@@ -12,6 +12,6 @@
     As an example, expand the FizzBuzz generator with the following behaviour:
     Numbers divisible by two are replaced by 'coconut' and numbers divisible 
     by seven are replaced by 'banana'."
-  :dependencies [[org.clojure/clojure "1.2.0-beta1"]
-                 [org.clojure/clojure-contrib "1.2.0-beta1"]]
+  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
+                 [org.clojure/clojure-contrib "1.2.0-RC1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
